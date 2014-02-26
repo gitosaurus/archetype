0071baf7dee25590a15558f568d8a5a924e1b59b
Derek Jones <dtj@Cloudwalker.local>
2014-02-26 06:49:23 -0800

    More expression testing

--- archetype/TestExpression.cpp
+++ archetype/TestExpression.cpp
@@ -19,6 +19,14 @@ using namespace std;
 namespace archetype {
     ARCHETYPE_TEST_REGISTER(TestExpression);
     
+    inline Expression expr_from_str(string src_str) {
+        istringstream in(src_str);
+        SourceFile src("test", in);
+        TokenStream token_stream(src);
+        Expression expr = form_expr(token_stream);
+        return expr;
+    }
+    
     inline string as_prefix(Expression expr) {
         ostringstream out;
         expr->prefixDisplay(out);
@@ -26,14 +34,28 @@ namespace archetype {
     }
     
     void TestExpression::runTests_(ostream& out) {
-        string src_str = "3 + 4 * 5";
-        istringstream in(src_str);
-        SourceFile src("test", in);
-        TokenStream token_stream(src);
-        Expression expr = form_expr(token_stream);
-        ARCHETYPE_TEST(expr != nullptr);
+        Expression expr1 = expr_from_str("3 + 4 * 5");
+        ARCHETYPE_TEST(expr1 != nullptr);
         string expected1 = "(+ 3 (* 4 5))";
-        string actual1 = as_prefix(expr);
+        string actual1 = as_prefix(expr1);
         ARCHETYPE_TEST_EQUAL(actual1, expected1);
+        
+        Expression expr2 = expr_from_str("main.dobj.isARoom");
+        ARCHETYPE_TEST(expr2 != nullptr);
+        string expected2 = "(. (. main dobj) isARoom)";
+        string actual2 = as_prefix(expr2);
+        ARCHETYPE_TEST_EQUAL(actual2, expected2);
+        
+        Expression expr3 = expr_from_str("a &:= b +:= c *:= d -:= 5");
+        ARCHETYPE_TEST(expr3 != nullptr);
+        string expected3 = "(&:= a (+:= b (*:= c (-:= d 5))))";
+        string actual3 = as_prefix(expr3);
+        ARCHETYPE_TEST_EQUAL(actual3, expected3);
+        
+        Expression expr4 = expr_from_str("1 + 2 + 3");
+        ARCHETYPE_TEST(expr4 != nullptr);
+        string expected4 = "(+ (+ 1 2) 3)";
+        string actual4 = as_prefix(expr4);
+        ARCHETYPE_TEST_EQUAL(actual4, expected4);
     }
 }
