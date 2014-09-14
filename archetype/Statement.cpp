2ff4584f0d86ff434121739a2f4f7dfe9f29ab88
Derek T. Jones <dtj@mac.com>
2014-09-14 16:16:13 -0700

    Oops! Structures containing unique_ptr need to be move()’d

--- archetype/Statement.cpp
+++ archetype/Statement.cpp
@@ -184,7 +184,7 @@ namespace archetype {
         for (int i = 0; i < entries; ++i) {
             Case case_pair;
             in >> case_pair.match >> case_pair.action;
-            cases_.push_back(case_pair);
+            cases_.push_back(move(case_pair));
         }
         int default_exists;
         in >> default_exists;
