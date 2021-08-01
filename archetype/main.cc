7f372d79da536d1f1629e2c5e3b1c73333b4d3d0
Derek T. Jones <dtj@mac.com>
2021-07-31 22:55:42 -0700

    Support single-command updates to an ACX file.
    Enables a way of playing an adventure where the game is steady-
    state saved.

--- archetype/main.cc
+++ archetype/main.cc
@@ -24,21 +24,34 @@
 #include "Wellspring.hh"
 
 namespace archetype {
-    static const char VersionString[] = "2.0";
+    static const char VersionString[] = "3.0";
 
     class Session {
     public:
+        Session():
+        silent_{false}
+        { }
+
+        void silent(bool value) {
+            silent_ = value;
+        }
+
         ~Session() {
-            UserOutput output = Universe::instance().output();
-            output->endLine();
-            output->put("Archetype ");
-            output->put(VersionString);
-            output->endLine();
+            if (not silent_) {
+                UserOutput output = Universe::instance().output();
+                output->endLine();
+                output->put("Archetype ");
+                output->put(VersionString);
+                output->endLine();
+            }
             TestRegistry::destroy();
             Universe::destroy();
             Wellspring::destroy();
             Keywords::destroy();
         }
+
+    private:
+        bool silent_;
     };
 
 }
@@ -52,14 +65,30 @@ void usage() {
         << endl
         << " --help                  Print this message and exit." << endl
         << " --test                  Run all test suites." << endl
-        << " --repl                  Enter the REPL (Read-Eval-Print Loop)" << endl
+        << " --repl                  Enter the REPL (Read-Eval-Print Loop)." << endl
+        << " --silent                Produce only game output and no other advisory output." << endl
         << " --source=file.ach       Read, compile, and run the given program." << endl
-        << "   --create[=file.acx]   Write the program given with --source to a binary file." << endl
-        << " --perform=file.acx      Load the saved binary file and send 'START' -> main." << endl
+        << "   --create[=file.acx]   Don't run, but write the program given by --source to a binary file." << endl
+        << " --perform=file.acx      Load a saved binary file and send 'START' -> main." << endl
+        << " --update=file.acx       Load binary, send 'UPDATE' -> main, save resulting binary to the same file." << endl
     ;
 }
 
-Value run_universe() {
+Value dispatch_to_universe(string message) {
+    ObjectPtr main_object = Universe::instance().getObject("main");
+    if (not main_object) {
+        throw invalid_argument("No 'main' object");
+    }
+    int start_id = Universe::instance().Messages.index(message);
+    Value start{new MessageValue{start_id}};
+    Value result = Object::send(main_object, move(start));
+    if (result->isSameValueAs(Value{new AbsentValue})) {
+        throw invalid_argument("No method for '" + message + "' on main object");
+    }
+    return result;
+}
+
+Value update_universe() {
     const string message = "START";
     ObjectPtr main_object = Universe::instance().getObject("main");
     if (not main_object) {
@@ -100,6 +129,9 @@ int main(int argc, const char* argv[]) {
         usage();
         return 0;
     }
+    if (opts.count("silent")) {
+        session.silent(true);
+    }
     if (opts.count("test")) {
         bool success = TestRegistry::instance().runAllTestSuites(cout);
         int exit_code = success ? 0 : 1;
@@ -123,7 +155,7 @@ int main(int argc, const char* argv[]) {
             }
             Universe::instance().reportUndefinedIdentifiers();
             if (not opts.count("create")) {
-                run_universe();
+                dispatch_to_universe("START");
             } else {
                 string filename_out = opts["create"];
                 if (filename_out.empty()) {
@@ -165,12 +197,41 @@ int main(int argc, const char* argv[]) {
         }
         in >> Universe::instance();
         try {
-            run_universe();
+            dispatch_to_universe("START");
         } catch (const archetype::QuitGame&) {
             return 0;
         } catch (const std::exception& e) {
             cerr << "ERROR: " << e.what() << endl;
             return 1;
+        }
+        return 0;
+    } else if (opts.count("update")) {
+        string filename = opts["update"];
+        if (filename.rfind('.') == string::npos) {
+            filename += ".acx";
+        }
+        {
+            InFileStorage in(filename);
+            if (not in.ok()) {
+                cerr << "Cannot open " << filename << endl;
+                return 1;
+            }
+            in >> Universe::instance();
+        }
+        try {
+            dispatch_to_universe("UPDATE");
+        } catch (const archetype::QuitGame&) {
+            return 0;
+        } catch (const std::exception& e) {
+            cerr << "ERROR: " << e.what() << endl;
+            return 1;
+        }
+        {
+            OutFileStorage out(filename);
+            if (not out.ok()) {
+                cerr << "Cannot write to " << filename << endl;
+            }
+            out << Universe::instance();
         }
         return 0;
     }
